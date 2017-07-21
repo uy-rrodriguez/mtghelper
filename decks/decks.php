666c28538ec3dbfc3e4802887b947083b03d23ef
Ricardo RODRIGUEZ @Troc <r.rodriguez@troc.com>
2017-07-21 16:00:02 +0200

    Module deck : list deck files

--- decks/decks.php
+++ decks/decks.php
@@ -1,1 +1,1 @@
-<?php$files = scandir(".");foreach ($files as $f)    echo basename($f) . PHP_EOL;
+<?php$files = scandir(".");$res = array();foreach ($files as $f)    if (! in_array($f, array('.', '..')))        $res[] = basename($f);echo implode("|", $res);
