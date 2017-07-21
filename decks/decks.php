1bb945392d05b828e4042d75eaad4f945a416f64
Ricardo RODRIGUEZ @Troc <r.rodriguez@troc.com>
2017-07-21 16:02:32 +0200

    Module deck : list deck files

--- decks/decks.php
+++ decks/decks.php
@@ -1,1 +1,1 @@
-<?php$files = scandir(".");$res = array();foreach ($files as $f)    if (! in_array($f, array('.', '..')))        $res[] = basename($f);echo implode("|", $res);
+<?php$files = scandir(".");$res = array();foreach ($files as $f)    if ( preg_match("/\.json$/", $f) )        $res[] = basename($f);echo implode("|", $res);
